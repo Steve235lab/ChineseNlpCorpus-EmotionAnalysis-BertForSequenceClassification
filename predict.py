6d87a68e5da25452a406347d6808199ec20cfda9
Steve D. J <84221016+Steve235lab@users.noreply.github.com>
2022-10-12 18:06:40 +0800

    本地运行，训练与测试通过

--- predict.py
+++ predict.py
@@ -1,17 +1,21 @@
 # -*-coding:UTF-8 -*-
 import torch
 from transformers import BertConfig, BertForSequenceClassification, BertTokenizer
-import torch.nn.functional as F     # 激励函数都在这
+import torch.nn.functional as F  # 激励函数都在这
+
 
 def to_input_id(sentence_input):
-    return tokenizer.build_inputs_with_special_tokens(tokenizer.convert_tokens_to_ids(tokenizer.tokenize(sentence_input)))
+    return tokenizer.build_inputs_with_special_tokens(
+        tokenizer.convert_tokens_to_ids(tokenizer.tokenize(sentence_input)))
+
 
 if __name__ == "__main__":
 
     # load and init
     tokenizer = BertTokenizer(vocab_file='bert-base-chinese-vocab.txt')
     config = BertConfig.from_pretrained('trained_model/config.json')
-    model = BertForSequenceClassification.from_pretrained('trained_model/pytorch_model.bin', from_tf=bool('.ckpt' in 'bert-base-chinese'), config=config)
+    model = BertForSequenceClassification.from_pretrained('trained_model/pytorch_model.bin',
+                                                          from_tf=bool('.ckpt' in 'bert-base-chinese'), config=config)
     model.eval()
 
     print("請輸入句子")
@@ -24,8 +28,8 @@ if __name__ == "__main__":
     # predict時，因為沒有label所以沒有loss
     outputs = model(input_ids)
 
-    prediction = torch.max(F.softmax(outputs[0]), dim = 1)[1] # 在第1維度取最大值並返回索引值 
-    predict_label = prediction.data.cpu().numpy().squeeze()   # 降維
+    prediction = torch.max(F.softmax(outputs[0]), dim=1)[1]  # 在第1維度取最大值並返回索引值
+    predict_label = prediction.data.cpu().numpy().squeeze()  # 降維
 
     if str(predict_label) == "1":
         print("正面")
--- train.py
+++ train.py
@@ -5,26 +5,29 @@ from transformers import BertForSequenceClassification, AdamW
 import torch
 import os
 
+
 # 計算正確值，參考網站:https://zhuanlan.zhihu.com/p/57294358
 def compute_accuracy(y_pred, y_target):
     _, y_pred_indices = y_pred.max(dim=1)
     n_correct = torch.eq(y_pred_indices, y_target).sum().item()
     return n_correct / len(y_pred_indices) * 100
 
+
 if __name__ == "__main__":
 
     # 设置使用的GPU用法來源:https://www.cnblogs.com/darkknightzh/p/6591923.html
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     # set device
     device = torch.device("cuda")
+    # torch.cuda.current_device()
 
     train_data_feature = convert_data_to_feature('train_data.txt')
     test_data_feature = convert_data_to_feature('test_data.txt')
     train_dataset = makeDataset(train_data_feature)
     test_dataset = makeDataset(test_data_feature)
 
-    train_dataloader = DataLoader(train_dataset ,batch_size=4 ,shuffle=True)
-    test_dataloader = DataLoader(test_dataset ,batch_size=4 ,shuffle=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=2, shuffle=True)
 
     model = BertForSequenceClassification.from_pretrained('bert-base-chinese')
     model.to(device)
@@ -32,10 +35,11 @@ if __name__ == "__main__":
     # Prepare optimizer and schedule (linear warmup and decay)
     no_decay = ['bias', 'LayerNorm.weight']
     optimizer_grouped_parameters = [
-        {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.0},
+        {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+         'weight_decay': 0.0},
         {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-        ]
-    Learning_rate = 5e-6       # 學習率
+    ]
+    Learning_rate = 5e-6  # 學習率
     optimizer = AdamW(optimizer_grouped_parameters, lr=Learning_rate, eps=1e-8)
 
     for epoch in range(5):
@@ -48,14 +52,14 @@ if __name__ == "__main__":
             batch_dict = tuple(t.to(device) for t in batch_dict)
 
             outputs = model(
-                input_ids = batch_dict[0],
-                token_type_ids = batch_dict[1],             # segment_ids
-                attention_mask = batch_dict[2],             # position_ids
-                labels = batch_dict[3]
-                )
+                input_ids=batch_dict[0],
+                token_type_ids=batch_dict[1],  # segment_ids
+                attention_mask=batch_dict[2],  # position_ids
+                labels=batch_dict[3]
+            )
             loss, logits = outputs[:2]
-            
-            train_correct = compute_accuracy(logits, batch_dict[3])       # 計算正確率
+
+            train_correct = compute_accuracy(logits, batch_dict[3])  # 計算正確率
             All_train_correct += train_correct
             AllTrainLoss += loss.item()
             count += 1
@@ -63,9 +67,9 @@ if __name__ == "__main__":
             model.zero_grad()
             loss.backward()
             optimizer.step()
-            
-        Average_train_correct = round(All_train_correct/count, 3)
-        Average_train_loss = round(AllTrainLoss/count, 3)
+
+        Average_train_correct = round(All_train_correct / count, 3)
+        Average_train_loss = round(AllTrainLoss / count, 3)
 
         # 測試模式
         model.eval()
@@ -76,25 +80,26 @@ if __name__ == "__main__":
             batch_dict = tuple(t.to(device) for t in batch_dict)
 
             outputs = model(
-                input_ids = batch_dict[0],
-                token_type_ids = batch_dict[1],             # segment_ids
-                attention_mask = batch_dict[2],             # position_ids
-                labels = batch_dict[3]
-                )
+                input_ids=batch_dict[0],
+                token_type_ids=batch_dict[1],  # segment_ids
+                attention_mask=batch_dict[2],  # position_ids
+                labels=batch_dict[3]
+            )
             loss, logits = outputs[:2]
 
-            test_correct = compute_accuracy(logits, batch_dict[3])       # 計算正確率
+            test_correct = compute_accuracy(logits, batch_dict[3])  # 計算正確率
             All_test_correct += test_correct
             AllTestLoss += loss.item()
 
             count += 1
-        
-        Average_test_correct = round(All_test_correct/count, 3)
-        Average_test_loss = round(AllTestLoss/count, 3)
 
-        print('第' + str(epoch+1) + '次' + '訓練模式，loss為:' + str(Average_train_loss) + ' 正確率為' + str(Average_train_correct)+ '，測試模式，loss為:' + str(Average_test_loss) + ' 正確率為' + str(Average_test_correct))
-    
+        Average_test_correct = round(All_test_correct / count, 3)
+        Average_test_loss = round(AllTestLoss / count, 3)
+
+        print('第' + str(epoch + 1) + '次' + '訓練模式，loss為:' + str(Average_train_loss) + ' 正確率為' + str(
+            Average_train_correct) + '，測試模式，loss為:' + str(Average_test_loss) + ' 正確率為' + str(
+            Average_test_correct))
+
     # 模型存檔
     model_to_save = model.module if hasattr(model, 'module') else model
     model_to_save.save_pretrained('trained_model')
-    
